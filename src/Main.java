48162c07696fb0cc416aab9cce23ceaf78c639ee
D.S <64180264+flstudio4@users.noreply.github.com>
2023-01-24 23:30:31 -0600

    Major refactoring

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,13 @@
  * <p>
  * This class contains main logic
  * <p>
+ *
  * @author Dmitrii Sumenko
  * Date: 10/12/2022
  */
 
 
 import java.util.ArrayList;
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -24,166 +24,172 @@ public class Main {
     private static final String[] departments = {"CIS", "HIS", "MAT", "LIT", "PHY", "ART", "BIO", "ENG"};
     private static int globalId = 1;
     private static final int TOTAL_CLASSES_ALLOWED = 5;
-    public static Scanner input;
+    public static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        input = new Scanner(System.in);
-
-        int choice = 0;     // Main switch control var
-        int choice2 = 0;    // Teachers' menu control var
-        int choice3 = 0;    // Students' menu control var
-        int choice4 = 0;    // Add more classes or not var
-        int choice5 = 0;    // Student delete control var
-        int choice6 = 0;    // Teacher delete control var
-        int total = 0;  // Total classes count control var
+        int choice;
 
         printGreeting();
 
         do {
-
             printMainMenu();
-            try {
-                choice = input.nextInt();
-            } catch (InputMismatchException e) {
-                System.out.println("Enter numeric value");
-            }
+            choice = integerInputValidation();
             input.nextLine();
-
-            switch (choice) {
-                case 0 -> printDemo();
-                case 1 -> {
-                    do {
-                        printTeacherMenu();
-                        try {
-                            choice2 = input.nextInt();
-                        } catch (InputMismatchException e) {
-                            System.out.println("Enter numeric value");
-                        }
-                        input.nextLine();
-
-                        if (choice2 == 0) {
-                            // creating a new Teacher and adding to database
-                            addEmployee(createTeacher());
-                            System.out.println("Teacher successfully added.\n");
-                            globalId++;
-                        } else if (choice2 == 1) {
-                            // Deleting a Teacher
-                            System.out.print("Enter Teacher ID to delete: ");
-                            try {
-                                choice6 = input.nextInt();
-                            } catch (InputMismatchException e) {
-                                System.out.println("Input numeric value");
-                            }
-                            input.nextLine();
-
-                            if (!teachers.isEmpty()) {
-                                int index = 0;
-                                for (Teacher teacher : teachers) {
-                                    if (teacher.getId() == choice6) {
-                                        //teachers.remove(teacher);
-                                        index = teachers.indexOf(teacher);
-                                        break;
-                                    }
-                                }
-                                teachers.remove(index);
-                                System.out.println("Teacher successfully deleted.\n");
-                            } else
-                                System.out.println("<<<<<< Deleting error...Teacher with id: " + choice6 + " does not exist >>>>>>\n");
-                        } else if (choice2 == 2) {
-                            // Printing all the Teachers
-                            if (!teachers.isEmpty()) {
-                                for (Teacher teacher : teachers) {
-                                    teacher.printEmployee(teacher);
-                                }
-                            } else System.out.println("<<<<<<<< Teachers list is empty yet >>>>>>>\n");
-                        }
-
-                    } while (choice2 != 3);
-                }
-                case 2 -> {
-                    do {
-                        printStudentMenu();
-                        try {
-                            choice3 = input.nextInt();
-                        } catch (InputMismatchException e) {
-                            System.out.println("Enter numeric value");
-                        }
-                        input.nextLine();
-
-                        if (choice3 == 0) {
-                            //Adding a Student
-                            addEmployee(createStudent());
-                            globalId++;
-                            System.out.println("Add some courses: ");
-                            addCourse(students.get(students.size() - 1).getClasses(), createCourse());
-
-                            do {
-                                System.out.println("Want to add more classes? (1 - yes, 2 - no)");
-                                try {
-                                    choice4 = input.nextInt();
-                                } catch (InputMismatchException e) {
-                                    System.out.println("Enter numeric value");
-                                }
-                                input.nextLine();
-
-                                if (choice4 == 1) {
-                                    addCourse(students.get(students.size() - 1).getClasses(), createCourse());
-                                } else if (choice4 == 2) {
-                                    break;
-                                }
-                                // cannot add more than 5 classes
-
-                                for (int i = 0; i < students.size(); i++) {
-                                    total = students.get(students.size() - 1).getClasses().size();
-                                }
-                            } while (total < TOTAL_CLASSES_ALLOWED && choice4 < 1 || choice4 > 2);
-
-                            System.out.println("Student successfully added.\n");
-                        } else if (choice3 == 1) {
-                            System.out.print("Enter Student ID to delete: ");
-                            try {
-                                choice5 = input.nextInt();
-                            } catch (InputMismatchException e) {
-                                System.out.println("Enter numeric value");
-                            }
-                            input.nextLine();
-
-                            // Deleting a Student
-                            if (!students.isEmpty()) {
-                                int index = 0;
-                                for (Student student : students) {
-                                    if (student.getId() == choice5) {
-                                        index = students.indexOf(student);
-                                        break;
-                                    }
-                                }
-                                students.remove(index);
-                                System.out.println("Student successfully deleted.\n");
-                            } else
-                                System.out.println("<<<<<< Deleting error...Student with id: " + choice5 + " does not exist >>>>>>\n");
-
-                        } else if (choice3 == 2) {
-                            if (!students.isEmpty()) {
-                                for (Student student : students) {
-                                    student.calculateTotalCredits(student);
-                                    student.calculateDue(student);
-                                }
-                                // Printing all the Students
-                                for (Student student : students) {
-                                    student.printEmployee(student);
-                                }
-                            } else System.out.println("<<<<< There are no students yet >>>>>>\n");
-                        }
-
-                    } while (choice3 != 3);
-                }
-                case 3 -> System.out.println("End of the program, exiting...");
-                default -> System.out.println("Some error occurred");
-            }
-
+            mainLogic(choice);
         } while (choice != 3);
 
         input.close();
+    }
+
+    public static int integerInputValidation() {
+        int value;
+        while (!input.hasNextInt()) {
+            System.out.println("Enter numeric value");
+            input.next();
+        }
+        value = input.nextInt();
+        return value;
+    }
+
+    public static void addingStudentLogic() {
+        int choice;
+        int total = 0;
+
+        addEmployee(createStudent());
+        globalId++;
+        System.out.println("Add some courses: ");
+        addCourse(students.get(students.size() - 1).getClasses(), createCourse());
+
+        do {
+            System.out.println("Want to add more classes? (1 - yes, 2 - no)");
+            choice = integerInputValidation();
+            input.nextLine();
+
+            if (choice == 1) {
+                addCourse(students.get(students.size() - 1).getClasses(), createCourse());
+            } else if (choice == 2) {
+                break;
+            }
+            // cannot add more than 5 classes
+
+            for (int i = 0; i < students.size(); i++) {
+                total = students.get(students.size() - 1).getClasses().size();
+            }
+        } while (total < TOTAL_CLASSES_ALLOWED && choice < 1 || choice > 2);
+
+        System.out.println("Student successfully added.\n");
+    }
+
+    public static void deletingStudentLogic() {
+        int choice;
+
+        System.out.print("Enter Student ID to delete: ");
+        choice = integerInputValidation();
+        input.nextLine();
+
+        int index = 0;
+
+        if (!students.isEmpty()) {
+            for (Student student : students) {
+                if (student.getId() == choice) {
+                    index = students.indexOf(student);
+                    break;
+                }
+            }
+            students.remove(index);
+            System.out.println("Student successfully deleted.\n");
+        } else
+            System.out.println("<<<<<< Deleting error...Student with id: " + choice + " does not exist >>>>>>\n");
+    }
+
+    public static void printingStudentsLogic() {
+        if (!students.isEmpty()) {
+            for (Student student : students) {
+                student.calculateTotalCredits(student);
+                student.calculateDue(student);
+            }
+            // Printing all the Students
+            for (Student student : students) {
+                student.printEmployee(student);
+            }
+        } else System.out.println("<<<<< There are no students yet >>>>>>\n");
+    }
+
+    public static void studentLogic() {
+        int choice;
+        do {
+            printStudentMenu();
+            choice = integerInputValidation();
+            input.nextLine();
+
+            switch (choice) {
+                case 0 -> addingStudentLogic();
+                case 1 -> deletingStudentLogic();
+                case 2 -> printingStudentsLogic();
+            }
+
+        } while (choice != 3);
+    }
+
+    public static void teacherLogic() {
+        int choice;
+
+        do {
+            printTeacherMenu();
+            choice = integerInputValidation();
+            input.nextLine();
+
+            switch (choice) {
+                case 0 -> creatingTeacherLogic();
+                case 1 -> deletingTeacherLogic();
+                case 2 -> printingTeachersLogic();
+            }
+
+        } while (choice != 3);
+    }
+
+    public static void creatingTeacherLogic() {
+        addEmployee(createTeacher());
+        System.out.println("Teacher successfully added.\n");
+        globalId++;
+    }
+
+    public static void deletingTeacherLogic() {
+        int choice;
+        System.out.print("Enter Teacher ID to delete: ");
+        choice = integerInputValidation();
+        input.nextLine();
+
+        if (!teachers.isEmpty()) {
+            int index = 0;
+            for (Teacher teacher : teachers) {
+                if (teacher.getId() == choice) {
+                    index = teachers.indexOf(teacher);
+                    break;
+                }
+            }
+            teachers.remove(index);
+            System.out.println("Teacher successfully deleted.\n");
+        } else
+            System.out.println("<<<<<< Deleting error...Teacher with id: " + choice + " does not exist >>>>>>\n");
+    }
+
+    public static void printingTeachersLogic() {
+        if (!teachers.isEmpty()) {
+            for (Teacher teacher : teachers) {
+                teacher.printEmployee(teacher);
+            }
+        } else System.out.println("<<<<<<<< Teachers list is empty yet >>>>>>>\n");
+    }
+
+    public static void mainLogic(int choice) {
+        switch (choice) {
+            case 0 -> printDemo();
+            case 1 -> teacherLogic();
+            case 2 -> studentLogic();
+            case 3 -> System.out.println("End of the program, exiting...");
+            default -> System.out.println("Some error occurred");
+        }
     }
 
     public static void addEmployee(Student student) {
@@ -215,72 +221,49 @@ public class Main {
     public static Teacher createTeacher() {
         String first;
         String last;
-        int dept = 0;
-        int exp = 0;
+        int dept;
+        int exp;
 
         do {
             System.out.println("Enter First Name: ");
             first = input.nextLine();
-
         } while (first.equals(""));
 
         do {
             System.out.println("Enter Last Name: ");
             last = input.nextLine();
-
         } while (last.equals(""));
 
         do {
             System.out.println("Enter Department: (0-CIS, 1-HIS, 2-MAT, 3-LIT, 4-PHY, 5-ART, 6-BIO, 7-ENG)");
-            try {
-                dept = input.nextInt();
-            } catch (InputMismatchException e) {
-                System.out.println("Enter numeric value");
-            }
+            dept = integerInputValidation();
             input.nextLine();
-
         } while (dept < 0 || dept > 7);
 
         do {
             System.out.println("Enter Years of Experience: 0 - 50: ");
-            try {
-                exp = input.nextInt();
-            } catch (InputMismatchException e) {
-                System.out.println("Input numeric value");
-            }
+            exp = integerInputValidation();
             input.nextLine();
-
         } while (exp < 0);
 
         return new Teacher(globalId, first, last, departments[dept], exp);
     }
 
     public static Course createCourse() {
-        int dept = 0;
-        int number = 0;
+        int dept;
+        int number;
         int credit;
 
         do {
-
             System.out.print("Enter number for course(0-CIS, 1-HIS, 2-MAT, 3-LIT, 4-PHY, 5-ART, 6-BIO, 7-ENG): ");
-            try {
-                dept = input.nextInt();
-            } catch (InputMismatchException e) {
-                System.out.println("Input numeric value");
-            }
+            dept = integerInputValidation();
             input.nextLine();
-
         } while (dept < 0 || dept > 7);
 
         do {
             System.out.print("Enter number for class(0-101, 1-121, 2-140, 3-144, 4-181, 5-244, 6-281, 7-302): ");
-            try {
-                number = input.nextInt();
-            } catch (InputMismatchException e) {
-                System.out.println("Input numeric value");
-            }
+            number = integerInputValidation();
             input.nextLine();
-
         } while (number < 0 || number > 7);
 
         credit = credits[number];
@@ -297,7 +280,6 @@ public class Main {
 
     public static void printDemo() {
         Demo.createInstances();
-
         Demo.students.get(0).makePayment(Demo.students.get(0), 1950);
         for (Employee employee : Demo.students) {
             employee.printEmployee(employee);
@@ -314,7 +296,6 @@ public class Main {
         String welcome = "***************** WELCOME TO **********************";
         String college = "***** COLLEGE EMPLOYEE MANAGEMENT APPLICATION *****";
         String version = "************* BETA VERSION 1.0 ********************";
-
         System.out.println();
         System.out.println(stars);
         System.out.println(welcome);
